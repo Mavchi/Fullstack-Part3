396d585c1fa2cc66404d87814f9cfdced7ad8460
Mavchi <aleksi.khlebnikov@gmail.com>
2022-05-26 15:29:10 +0300

    3.2: puhelinluettelon backend step2 done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 
@@ -28,6 +29,10 @@ let persons = [
 
 app.get('/', (request, response) => {
     response.send("<h1>Puhelinluettelon palvelin-koodi</h1>")
+})
+
+app.get('/info', (request, response) => {
+    response.send(`Phonebook has info for ${persons.length} people<br/><br/>${new Date().toUTCString()}`)
 })
 
 app.get('/api/persons', (request, response) => {
