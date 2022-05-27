d378fa4d196b2677197a8751f69929d92c9e5b9a
Mavchi <aleksi.khlebnikov@gmail.com>
2022-05-27 10:54:24 +0300

    .5: puhelinluettelon backend step5 done

--- index.js
+++ index.js
@@ -42,12 +42,36 @@ app.get('/api/persons', (request, response) => {
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     const person = persons.find(p => p.id === id)
-    console.log(person)
+    //console.log(person)
     if (person) {
         response.json(person)
     } else {
         response.status(404).end()
     }
+})
+
+app.post('/api/persons', (request, reponse) => {
+    const body = request.body
+    if (!body.name){
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random() * 10000),
+    }
+    persons = persons.concat(person)
+    response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
 })
 
 const PORT = 3001
